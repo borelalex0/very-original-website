e045c929eac17acbc3c9e6a4cbe6d9fc53d4a21c
Alex Borel <borelalex0@gmail.com>
2022-10-20 10:18:55 -0400

    Added some JS functionality, updated styles, updated HTML content

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function createElements(elementType, n/*number of elements*/) {
+    let k = document.createElement(elementType);
+    for (let i = 0; i < n; i++) {
+        let k = document.createElement(elementType);
+        k.textContent = `This is ${elementType} ${i+1}`;
+        document.body.append(k);
+    }
+    return 0;
+}
+
 const delement = document.createElement("div");
 const belement = document.querySelector("body");
 
@@ -5,4 +15,4 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content created by JavaScript";
 
 delement.appendChild(h2);
-belement.appendChild(delement);
+document.body.appendChild(delement);
