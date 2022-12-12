7db28bd7b6575351c4eda9035e88a56297ce0600
Alex Borel <borelalex0@gmail.com>
2022-12-12 16:29:02 -0500

    added console logs for debugging

--- style-sheet.js
+++ style-sheet.js
@@ -7,7 +7,16 @@ const lightModeButton = document.getElementById("lightModeButton");
 const darkModeButton = document.getElementById("darkModeButton");
 const basicModeButton = document.getElementById("basicModeButton");
 
-lightModeButton.onclick = function () {swapStyleSheet('./css/light-basic.css')}
-darkModeButton.onclick = function () {swapStyleSheet('./css/dark-basic.css')}
-basicModeButton.onclick = function () {swapStyleSheet('./css/basic.css')}
+lightModeButton.onclick = function () {
+    console.log('light mode');
+    swapStyleSheet('./css/light-basic.css');
+}
+darkModeButton.onclick = function () {
+    console.log('darkmode');
+    swapStyleSheet('./css/dark-basic.css');
+}
+basicModeButton.onclick = function () {
+    console.log('basic');
+    swapStyleSheet('./css/basic.css');
+}
 /* End Style Sheet Change Functionality */
