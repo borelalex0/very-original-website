d3fccd6a381936186d841ffd05f0ceae61f7e739
Alex Borel <borelalex0@gmail.com>
2022-12-14 10:27:34 -0500

    added vertical and horizontal nav bars, js and css.

--- script.js
+++ script.js
@@ -13,15 +13,40 @@ createElements('h2', 3, 'success!');
 
 
 
-/* function that reads content of an element, if it has children, recurse, if not, log as html content. */
-function readHtml() {
-    let htmlObject = {
-        name: document
-    }
-    if (htmlObject.name.hasChildNodes) {
-        let childNodes = parent.name
+
+let cliDropDown = ['ls','cd','mkdir','rmdir','rm','mv'];
+let cliDropDownInsertID = 'cli-blog-section';
+insertDropDown(cliDropDown, cliDropDownInsertID);
+
+function insertDropDown(dropDownList, insertAtID) {
+    let parent1 = document.getElementById(insertAtID);
+    let tempul = document.createElement("ul");
+    tempul.className = "nav-bar";
+    tempul.id = "cliDropDownList";
+    document.getElementById(insertAtID).append(tempul);
+    console.log(`appended element id=${tempul}`);
+    for (let i = 0; i < dropDownList.length; i++) {
+        console.log(`loop ${i}`);
+        let templi = document.createElement("li");
+        let tempa = document.createElement("a");
+        templi.className = "nav-bar-item-vertical";
+        tempa.className = "nav-bar-link";
+        tempa.id = dropDownList[i] + "DropDownButton";
+        tempa.innerHTML = dropDownList[i];
+        tempul.append(templi);
+        templi.append(tempa);
+        console.log(`appended element id=${tempa.id}`);
     }
 }
+
+/* 
+const lsDropDownButton = document.getElementById("lsDropDownButton");
+const lsDropDownButton = document.getElementById("cdDropDownButton");
+const mkdirDropDownButton = document.getElementById("mkdirDropDownButton");
+const lsDropDownButton = document.getElementById("lsDropDownButton");
+const lsDropDownButton = document.getElementById("lsDropDownButton");
+const lsDropDownButton = document.getElementById("lsDropDownButton"); 
+*/
 
 /* recreate index.hmtl as an object with nested elements */
 let exampleObject = {
