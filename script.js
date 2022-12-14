351db4d86eae9c20c37acbc4050388a1743657b5
Alex Borel <borelalex0@gmail.com>
2022-12-14 15:13:17 -0500

    removed import statments to allow loading without breaking CORS

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-import {createElements, insertDropDown} from './tools.js'
+/* 
+Turn scipt with import into type="module" in html when turning this on.
+This will break CORS resulting in errors running this js file 
+when loading outside of https.
 
-/* document.onload = () => {
+import {createElements, insertDropDown} from './tools.js'
+document.onload = () => {
     let cliDropDown = ['ls','cd','mkdir','rmdir','rm','mv'];
     let cliDropDownInsertID = 'cli-blog-section';
     insertDropDown(cliDropDown, cliDropDownInsertID);
 
-} */
+} 
+*/
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.body.onload = () => {
-        let cliDropDown = ['ls','cd','mkdir','rmdir','rm','mv'];
-        let cliDropDownInsertID = 'cli-blog-section';
-        insertDropDown(cliDropDown, cliDropDownInsertID);
-    
+function insertDropDown(dropDownList, insertAtID) {
+    let parent1 = document.getElementById(insertAtID);
+    let tempul = document.createElement("ul");
+    tempul.className = "nav-bar";
+    tempul.id = "cliDropDownList";
+    document.getElementById(insertAtID).append(tempul);
+    console.log(`appended element id=${tempul}`);
+    for (let i = 0; i < dropDownList.length; i++) {
+        console.log(`loop ${i}`);
+        let templi = document.createElement("li");
+        let tempa = document.createElement("a");
+        templi.className = "nav-bar-item-vertical";
+        tempa.className = "nav-bar-link";
+        tempa.id = dropDownList[i] + "DropDownButton";
+        tempa.innerHTML = dropDownList[i];
+        tempul.append(templi);
+        templi.append(tempa);
+        console.log(`appended element id=${tempa.id}`);
     }
+}
+
+// creates dropdown list once all html content is loaded.
+document.addEventListener('DOMContentLoaded', () => {
+        let cliDropDown = ['ls','cd','mkdir','rmdir','rm','mv'];
+        insertDropDown(cliDropDown, 'cli-blog-section');
 });
 
-let element = document.body;
-element.html
-
-/* const delement = document.createElement("div");
+/*
+const delement = document.createElement("div");
 delement.style.textAlign = "center"
 
 const h2 = document.createElement("h2");
@@ -28,70 +49,5 @@ h2.textContent = "This content created by JavaScript";
 delement.appendChild(h2);
 document.body.appendChild(delement);
 
-createElements('h2', 3, 'success!'); */
-
-/* recreate index.hmtl as an object with nested elements */
-let exampleObject = {
-    name: 'main',
-    children: [
-        {
-            name: 'doctype',
-            children: []
-        },
-        {
-            name: 'html',
-            children: [
-                {
-                    name: 'head',
-                    children: [
-                        {
-                            name: 'meta',
-                            children: [],
-                            attributes: 
-                                {
-                                    charset: 'utf-8'
-                                }
-                        },
-                        {
-                            name: 'meta',
-                            children: [],
-                            attributes:
-                                {
-                                    name: 'viewport',
-                                    content: 'width=device-width, initial-scale=1.0'
-                                }
-                        
-                        },
-                        {
-                            name: 'style',
-                            children: [],
-                            attributes: {},
-                            textContent: "@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400&display=swap');"
-                        },
-                        {
-                            name: 'link',
-                            children: [],
-                            attributes: {
-                                id: 'pagestyle',
-                                rel: 'stylesheet',
-                                href: './css/basic.css'
-                            }
-                        },
-                        {
-                            name: 'title',
-                            textContent: "very original website",
-                        }
-                    ]
-                },
-                {
-                    name: 'body',
-                    children: [
-                        {
-                            name: 'script'
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-    }
+createElements('h2', 3, 'success!');
+*/
