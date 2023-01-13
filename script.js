a42c6b93e0a372fc86d7ef23846db412eec05f0e
Alex Borel <borelalex0@gmail.com>
2023-01-13 17:43:43 -0500

    css changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* 
-Turn scipt with import into type="module" in html when turning this on.
+Turn script with import into type="module" in html when turning this on.
 This will break CORS resulting in errors running this js file 
 when loading outside of https.
 
@@ -33,10 +33,34 @@ function insertDropDown(dropDownList, insertAtID) {
     }
 }
 
+function extendDropDown() {
+    // declare the link as a variable
+    let acc = document.getElementsByClassName("nav-bar-item-vertical");
+    console.log(acc);
+    console.log(acc.length);
+    for (let i = 0; i < acc.length; i++) {
+        console.log(`Iteration: ${i}`);
+        acc[i].addEventListener("click", function() {
+          /* Toggle between adding and removing the "active" class,
+          to highlight the button that controls the panel */
+          this.classList.toggle("active");
+      
+          /* Toggle between hiding and showing the active panel */
+          var panel = this.nextElementSibling;
+          if (panel.style.display === "block") {
+            panel.style.display = "none";
+          } else {
+            panel.style.display = "block";
+          }
+        });
+      }
+}
+
 // creates dropdown list once all html content is loaded.
 document.addEventListener('DOMContentLoaded', () => {
         let cliDropDown = ['ls','cd','mkdir','rmdir','rm','mv'];
         insertDropDown(cliDropDown, 'cli-blog-section');
+        extendDropDown();
 });
 
 /*
